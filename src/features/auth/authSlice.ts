9c632fc05d3ab084c195c7098fd3bf525f5050ea
TKMhub <111867865+TKMhub@users.noreply.github.com>
2023-07-23 23:36:24 +0900

    init

---
+++ src/features/auth/authSlice.ts
@@ -0,0 +1,18 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { RootState } from "../../app/store";
+import axios from "axios";
+import { PROPS_AUTHEN, PROPS_PROFILE, PROPS_NICKNAME } from "../types";
+
+const apiUrl = process.env.REACT_APP_DEV_API_URL;
+
+export const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {},
+});
+
+export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+
+export const selectCount = (state: RootState) => state.counter.value;
+
+export default counterSlice.reducer;
