29a84fc65c7dd9135159dab3bdf094eadbb7d593
TKMhub <111867865+TKMhub@users.noreply.github.com>
2023-07-29 11:52:42 +0900

    core

---
+++ src/features/core/Core.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Auth from "../auth/auth";
+
+const Core: React.FC = () => {
+  return (
+    <div>
+      <Auth />
+    </div>
+  );
+};
+
+export default Core;
--- src/features/types.ts
+++ src/features/types.ts
@@ -22,24 +22,21 @@ export interface PROPS_NICKNAME {
 }
 
 /**
-File インターフェース:
-これは Blob インターフェースを継承しており、ファイルの属性を表すために使用されます。ファイルには以下の属性が含まれます：
-
-lastModified: ファイルの最終変更日時を表す数値です。
-name: ファイルの名前を表す文字列です。
-PROPS_AUTHEN インターフェース:
-ユーザーの認証情報を表すためのインターフェースです。以下の属性が含まれます：
-
-email: ユーザーのメールアドレスを表す文字列です。
-password: ユーザーのパスワードを表す文字列です。
-PROPS_PROFILE インターフェース:
-ユーザープロファイル情報を表すためのインターフェースです。以下の属性が含まれます：
-
-id: ユーザーのIDを表す数値です。
-nickName: ユーザーのニックネームを表す文字列です。
-img: ユーザーのプロフィール画像を表す File インスタンスまたはnullです。File インターフェースを使用してファイル情報を保持します。
-PROPS_NICKNAME インターフェース:
-ユーザーのニックネームを更新する際に使用されるインターフェースです。以下の属性が含まれます：
-
-nickName: 更新したいニックネームを表す文字列です。
+ * postSlice.ts
  */
+export interface PROPS_NEWPOST {
+  title: string;
+  img: File | null;
+}
+
+export interface PROPS_LIKED {
+  id: number;
+  title: string;
+  current: number[];
+  new: number;
+}
+
+export interface PROPS_COMMENT {
+  text: string;
+  post: number;
+}
